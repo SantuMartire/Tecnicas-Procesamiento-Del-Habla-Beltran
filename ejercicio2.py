b1bf5f7ba14883458a0687de631b346f59720f54
SantuMartire <141597207+SantuMartire@users.noreply.github.com>
2026-04-01 09:09:45 -0300

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+import nltk
+
+'''
+mobi=nltk.corpus.gutenberg.words("melville-moby_dick.txt")
+print(mobi)
+print(len(mobi))
+'''
+
+
+trump=nltk.corpus.inaugural.words("2025-Trump.txt")
+print(trump)
+print(len(trump))
+
