ed1aee35aea0f56a235d3bf27c0f92914197d578
SantuMartire <141597207+SantuMartire@users.noreply.github.com>
2026-04-01 11:10:32 -0300

    Add files via upload

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,10 +1,12 @@
+#pip install matplotlib
 import nltk
 
 from nltk.corpus.reader.plaintext import PlaintextCorpusReader
 from nltk import FreqDist
 corpus = PlaintextCorpusReader(".", 'melville-moby_dick.txt')
-
 frecuencia=FreqDist(corpus.words())
 
-for mc in frecuencia.most_common(50):
+for mc in frecuencia.most_common(20):
     print(mc)
+
+frecuencia.plot(20, show=True)
