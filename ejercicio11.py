53b93ebf00c6860d8c5a24c247fd7638ac525dec
SantuMartire <141597207+SantuMartire@users.noreply.github.com>
2026-04-01 10:58:03 -0300

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,8 @@
+from nltk.corpus.reader.plaintext import PlaintextCorpusReader
+from nltk import FreqDist
+corpus = PlaintextCorpusReader(".", 'melville-moby_dick.txt')
+
+frecuencia=FreqDist(corpus.words())
+
+for mc in frecuencia.most_common(50):
+    print(mc)
